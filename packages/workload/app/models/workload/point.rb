a54658fc832b57d7101f0fe79a6799b5a0c3b873
naokirin <naoki.rin186@gmail.com>
2024-04-23 18:43:39 +0900

    Add validation on workload points for future month

--- packages/workload/app/models/workload/point.rb
+++ packages/workload/app/models/workload/point.rb
@@ -5,4 +5,14 @@ class Workload::Point < ApplicationRecord
   belongs_to :user_account, class_name: 'User::Account'
 
   validates :value, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+  validates :date, presence: true
+  validate :date_cannot_be_future
+
+  private
+
+  def date_cannot_be_future
+    if date.present? && date.beginning_of_month.future?
+      errors.add(:date, "は未来の日付は指定できません")
+    end
+  end
 end
