ac63dcfd6c4825b8a2dc774ca84b83aa08fb5f1a
naokirin <naoki.rin186@gmail.com>
2024-04-29 23:32:00 +0900

    Fix dartsass configurations

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,14 +4,23 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-Dir.glob("packages/*/app/assets/*").each do |path|
+Dir.glob("packages/*/app/assets/builds/*").each do |path|
   Rails.application.config.assets.paths << path
 end
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += Dir.glob("packages/*/app/assets/**/*").select do |path|
-  %w(.js .css .scss .sass).include?(File.extname(path)) &&
+Rails.application.config.assets.precompile += Dir.glob("packages/*/app/assets/builds/**/*").select do |path|
+  %w(.js).include?(File.extname(path)) &&
     !File.basename(path).start_with?('_')
-end.map {|p| p.gsub(/\.s?(css|js)(\.erb|)$/, '.\1').gsub(/packages\/[^\/]+\/app\/assets\/[^\/]+\//, '') }
+end.map {|p| p.gsub(/\.s?(js)(\.erb|)$/, '.\1').gsub(/packages\/[^\/]+\/app\/assets\/[^\/]+\//, '') }
+
+Rails.application.config.dartsass.load_paths = Dir.glob("packages/*/app/assets/stylesheets/").map do |path|
+  Rails.root.join(path)
+end
+
+Rails.application.config.dartsass.builds = Dir.glob("packages/*/app/assets/stylesheets/*.scss").map do |path|
+  package_path = path.match(/packages\/[^\/]+/)[0]
+  [File.join('../../../', path), File.join(package_path, "#{File.basename(path, '.scss')}.css")]
+end.to_h
